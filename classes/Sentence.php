90f29512b1ce4570bf68bb133f5845d06b209d1b
Martijn <martijn@vanderlee.com>
2017-10-14 11:19:50 +0200

    Changed array definition

--- classes/Sentence.php
+++ classes/Sentence.php
@@ -52,7 +52,7 @@ class Sentence {
 	 * @param string $string
 	 * @param int $limit
 	 * @param int $flags
-	 * @return array<integer,integer|string>
+	 * @return array
 	 */
 	private static function mbSplit($pattern, $string, $limit = -1, $flags = 0)
 	{
